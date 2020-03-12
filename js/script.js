4ce3cc129cb5264084ea8a2995649f4098b7b759
LBSJebe <53866884+LBSJebe@users.noreply.github.com>
2020-03-12 10:03:48 +0100

    init commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const P = new Pokedex.Pokedex();
+
+P.getPokemonByName("bulbasaur")
+  .then(res => {
+    //om det blir rätt körs denna kod:
+    console.log(res);
+    document.getElementById("pokemon_name").innerHTML = res.name;
+    document.getElementById(
+      "pokemon_sprite"
+    ).innerHTML = `<img src="${res.sprites.front_default}"/>`;
+  })
+  .catch(err => {
+    //om det blir fel körs denna kod:
+    console.log(err);
+    document.getElementById("pokemon_name").innerHTML =
+      "No pokemon by that name";
+  });
