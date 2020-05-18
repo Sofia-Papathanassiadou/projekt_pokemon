254ca1879f09bba1328bc4fa29c374f529fbd4d2
Sofia-Papathanassiadou <Sofia-Papathanassiadou@users.noreply.github.com>
2020-05-18 15:56:24 +0200

    Lagt till stats + shiny

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const P = new Pokedex.Pokedex();
 
-P.getPokemonByName("59")
+  document.getElementById("SB").onclick=function(){
+   var PS= document.getElementById("Search").value;
+   P.getPokemonByName(PS)
   .then(res => {
     //om det blir rätt körs denna kod:
     console.log(res);
@@ -8,6 +10,11 @@ P.getPokemonByName("59")
     document.getElementById(
       "pokemon_sprite"
     ).innerHTML = `<img src="${res.sprites.front_default}"/>`;
+    document.getElementById(
+      "pokemon_sprite"
+    ).innerHTML += `<img src="${res.sprites.front_shiny}"/>`;
+    document.getElementById("pokemon_stats").innerHTML= res.stats[0].stat.name; 
+    document.getElementById("pokemon_stats").innerHTML+= "<br> " + res.stats[0].base_stat;
   })
   .catch(err => {
     //om det blir fel körs denna kod:
@@ -15,3 +22,5 @@ P.getPokemonByName("59")
     document.getElementById("pokemon_name").innerHTML =
       "No pokemon by that name";
   });
+  }
+
